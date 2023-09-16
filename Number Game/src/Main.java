2dc68e8fc2e8716d498e118bd920ec4a4ad23f71
Ziad Emad <115287958+1Ziad-Etman1@users.noreply.github.com>
2023-09-17 00:09:12 +0300

    Console version is done

--- Number Game/src/Main.java
+++ Number Game/src/Main.java
@@ -1,3 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
-    
+    Scanner in = new Scanner(System.in);
+    Random rand = new Random();
+    int Number = rand.nextInt(1,100);
+    System.out.println("Welcome to Guess Number Game!\n\n");
+    boolean flag = false;
+    int counter = 0;
+    while(!flag){
+        System.out.print("Your guess is : ");
+        int User_guess = in.nextInt();
+        counter++;
+        if(User_guess < Number){
+            System.out.println("No, Higher!");
+        } else if (User_guess > Number) {
+            System.out.println("No, Lower!");
+        } else {
+            System.out.println("Excellent!");
+            System.out.println("Your Score is: " + counter);
+            flag = true;
+        }
+    }
 }
